73e3a5c799e7b1592165d87bfd9d9f6819abacf2
Sean Butterworth <sean_butterworth@hotmail.com>
2019-05-27 10:48:49 -0700

    Test harness work
    Removed unneeded shaders
    General cleanup

--- Asgard/Framework.cpp
+++ Asgard/Framework.cpp
@@ -4,18 +4,11 @@
 #include <cwchar>
 #include <functional>
 #include <limits>
-#include <glm/gtc/matrix_transform.hpp>
+#include <glm\glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
 #include "Framework.h"
 #include "Platform.h"
 #include "Muspelheim.h"
-
-// https://stackoverflow.com/questions/41781135/sampling-integers-in-opengl-shader-doesnt-work-result-is-always-0
-// http://antongerdelan.net/opengl/vertexbuffers.html
-
-//glTexImage1D( GL_TEXTURE_1D, 0, GL_R8, ( temp ).size(), 0, GL_RED, GL_UNSIGNED_BYTE, &( temp )[ 0 ] );
-// glTexImage2D(GL_TEXTURE_2D, 0, GL_R16I, 512, 512, 0, GL_RED_INTEGER, GL_SHORT, data);
-// glTexImage1D(GL_TEXTURE_1D, 0, GL_R32I, myVec.size(), 0, GL_RED_INTEGER, GL_INT, &myVec[ 0 ]);
-// glTexImage1D(GL_TEXTURE_1D, 0, GL_RG16UI, 128, 0, GL_RG_INTEGER, GL_UNSIGNED_SHORT, data);
 
 #pragma comment(lib, "Muspelheim.lib")
 
@@ -52,12 +45,17 @@ bool Framework::Init( Platform::WindowHandle hWindow, const Platform::LaunchInfo
 
 			if ( 0 == shaderId || false == m_Renderer->SetSurfaceShader( m_MainScreenID, shaderId ) )
 			{
+				return false;
 			}
+
+			// Dark green : Handy color calculator : https://www.tydac.ch/color/
+			m_Renderer->SetSurfaceColor( m_MainScreenID, glm::vec4( 0.0f, 0.45f, 0.05f, 1.0f ) );
 
 			shaderId = m_Renderer->LoadShader( text2DShaderName );
 			
 			if ( 0 == shaderId || false == m_Renderer->SetSurfaceTextShader( m_MainScreenID, shaderId ) )
 			{
+				return false;
 			}
 		}
 
@@ -73,26 +71,29 @@ void Framework::Shutdown()
 	m_Renderer.reset();
 }
 
-void Framework::Update()
-{
-	if( m_Initialized )
-	{
-		m_Renderer->BeginRender( glm::mat4() );
-
-		wchar_t stringBuffer[ Platform::kMaxStringLength ];
-
-		std::swprintf( stringBuffer, Platform::kMaxStringLength, L"My mother and your mother were hanging up clothes." );
-		m_Renderer->DrawSurfaceString( m_MainScreenID, stringBuffer, 1, 1, Muspelheim::Renderer::TEXT_LEFT );
-
-		m_Renderer->EndRender();
-	}
-}
-
 void Framework::ResizeWindow( unsigned short width, unsigned short height )
 {
 	if( m_Initialized )
 	{
 		PLATFORM_ASSERT( nullptr != m_Renderer );
 		m_Renderer->SetWindowSize( width, height );
+	}
+}
+
+// Research
+void Framework::Update()
+{
+	if ( m_Initialized )
+	{
+		m_Renderer->BeginRender( glm::mat4() );
+
+		wchar_t stringBuffer[ Platform::kMaxStringLength ];
+
+		unsigned int const color(0xFFFFFFFF);
+
+		std::swprintf( stringBuffer, Platform::kMaxStringLength, L"My mother and your mother were hanging up clothes." );
+		m_Renderer->DrawSurfaceString( m_MainScreenID, stringBuffer, color, 1, 1, Muspelheim::Renderer::TEXT_LEFT );
+
+		m_Renderer->EndRender();
 	}
 }
