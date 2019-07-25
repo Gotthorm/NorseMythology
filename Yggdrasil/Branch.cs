b566f2f4dd2bb059498c9f7fa3f69282e219c2ea
Sean Butterworth <sean_butterworth@hotmail.com>
2019-07-24 23:41:59 -0700

    Added branch class
    Initial scoping of new branch class
    Added some of the initial API for the WorldData class

---
+++ Yggdrasil/Branch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yggdrasil
+{
+    // This represents a single imported data element
+    class Branch
+    {
+        // This should end up in a more global location?
+        public enum Type
+        {
+            Invalid,
+            Elevation
+        }
+
+        private uint m_Version = 0;
+
+        private string m_Remarks = "";
+        private string m_SourceURL = "";
+
+        private int m_ElevationMinimum = 0;
+        private int m_ElevationMaximum = 0;
+        private int m_ImageWidth = 0;
+        private int m_ImageHeight = 0;
+
+        
+        private float m_GlobalCoordinateWest = 0.0f;
+        private float m_GlobalCoordinateEast = 0.0f;
+        private float m_GlobalCoordinateNorth = 0.0f;
+        private float m_GlobalCoordinateSouth = 0.0f;
+
+        private Type m_Type = Type.Invalid;
+
+        private byte[] m_ImageData = null;
+    }
+}
--- Yggdrasil/WorldData.cs
+++ Yggdrasil/WorldData.cs
@@ -16,8 +16,14 @@ namespace Yggdrasil
         public int Height { get { return m_Height; } }
         public Image Image { get { return m_Image; } }
 
+        // Load a yggdrasil file.
         public bool Load(string filePath)
         {
+            // Erases all loaded data.
+
+            // The data loaded will be a table of data elements that will be used 
+            // to generate a live copy of world data
+
             m_Width = 0;
             m_Height = 0;
             m_Image = null;
@@ -51,6 +57,60 @@ namespace Yggdrasil
                 }
             }
 
+            return false;
+        }
+
+        // Save a yggdrasil file
+        public bool Save(string filePath)
+        {
+            return false;
+        }
+
+        // Import a single image, merging it into the current world data
+        public bool ImportImage(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                // Attempt to import the given file as image data
+                try
+                {
+                    using (MagickImage image = new MagickImage(filePath))
+                    {
+                        // Perform any extra verification here?
+
+                        // The image has been loaded
+
+                        // Extract the needed information
+
+                        // Add the image to our yggdrasil data
+
+                        int bitDepth = image.BitDepth();
+
+                        m_Width = image.Width;
+                        m_Height = image.Height;
+
+                        m_Image = image.ToBitmap();
+
+                        if (bitDepth == 8)
+                        {
+                            Console.WriteLine("8 Bit");
+                        }
+                    }
+
+                    return true;
+                }
+                catch (Exception)
+                {
+                    // Catch everything and ignore
+                }
+            }
+
+            return false;
+        }
+
+        // Exports the world data in a binary format that contains no external data references
+        public bool Export(string filePath)
+        {
             return false;
         }
 
